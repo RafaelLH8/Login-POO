4d8b2d77cb467b20ded8fe880b50f436a4221293
Rafael <57455645+RafaelLH8@users.noreply.github.com>
2020-08-29 19:42:58 -0300

    Add files via upload

---
+++ Login_Poo/classes/Noticia.php
@@ -0,0 +1,95 @@
+<?php
+
+class Noticia{
+    private $id;
+    private $titulo;
+    private $descricao;
+    private $comentarios;
+    private $data;
+    private $usuario;
+
+    public function setId($id){
+        $this->id = $id;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setTitulo($titulo){
+        $this->titulo = $titulo;
+    }
+
+    public function getTitulo(){
+        return $this->titulo;
+    }
+
+    public function setDescricao($descricao){
+        $this->descricao = $descricao;
+    }
+
+    public function getDescricao(){
+        return $this->descricao;
+    }
+
+    public function setComentarios($comentarios){
+        $this->comentarios = $comentarios;
+    }
+
+    public function getComentarios(){
+        return $this->comentarios;
+    }
+    public function setData($data){
+        $this->data = $data;
+    }
+
+        public function getData(){
+        return $this->data;
+    }
+    public function setUsuario($usuario){
+        $this->usuario = $usuario;
+    }
+
+    public function getUsuario(){
+        return $this->usuario;
+    }
+
+    public function index(){
+        $this->listar();
+    }
+
+    public function listar(){
+        $conexao = Conexao::getInstance();
+        $sql = "SELECT id, titulo, descricao, DATE_FORMAT(data, '%d/%m/%Y') AS data,
+        (SELECT nome FROM usuario WHERE id=noticia.usuario_id) AS nome_usuario
+        FROM noticia
+        ORDER BY id DESC LIMIT 5";
+
+        $resultado = $conexao -> query($sql);
+        $noticias = null;
+
+        while($noticia = $resultado->fetch(PDO::FETCH_OBJ)){
+            $noticias[] = $noticia;
+        }
+
+        include HOME_DIR."view/paginas/noticias/noticias.php";
+    }
+
+    public function ver($id){
+        $conexao = Conexao::getInstance();
+        $sql = "SELECT id, titulo, descricao, DATE_FORMAT(data, '%d/%m/%Y') AS data,
+        (SELECT nome FROM usuario WHERE id=noticia.usuario_id) AS nome_usuario
+        FROM noticia
+        WHERE id=".$id;
+
+        $resultado = $conexao -> query($sql);
+        $noticia = $resultado -> fetch(PDO::FETCH_OBJ);
+        include HOME_DIR."view/paginas/noticias/noticia.php";
+    }
+
+
+
+}
+
+
+?>
---
+++ Login_Poo/config.php
@@ -0,0 +1,13 @@
+<?php
+	session_start();
+	define("HOME_URI", "http://localhost/Login_Poo/");
+	define("HOME_DIR", "C:\\xampp\htdocs\Login_Poo\\");
+
+	define('SGBD','mysql');
+	define('HOST_DB', 'localhost');
+	define('DB', 'login');
+	define('USER_DB', 'root');
+	define('PASS_DB', '');
+
+	define("DEFAULT-PASS", "usuario");
+?>
